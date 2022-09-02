a26bbd5e44ae55715947311b5aa552a42d92a008
mukeshkapse <mukeshkapse1832@gmail.com>
2022-09-02 21:04:03 +0530

    Added: Initialization of game

--- src/com/bridgelabz/deckofcard/DeckOfCard.java
+++ src/com/bridgelabz/deckofcard/DeckOfCard.java
@@ -4,4 +4,8 @@ public class DeckOfCard {
     String[] deck = new String[52];
    String[] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
    String[] Rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Deck of card");
+    }
 }
