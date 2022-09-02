daa47417b4c62d9e9cdae33c2e7c51710420aacc
mukeshkapse <mukeshkapse1832@gmail.com>
2022-09-02 20:54:00 +0530

    Added: Deck of card program

---
+++ src/com/bridgelabz/deckofcard/DeckOfCard.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.deckofcard;
+
+public class DeckOfCard {
+    String[] deck = new String[52];
+   String[] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
+   String[] Rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+}
